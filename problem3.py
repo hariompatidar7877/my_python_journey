3cb937cd4a42f2df5415d164bffe770eac9d5ec7
HARIOM PATIDAR <149182990+hariompatidar7877@users.noreply.github.com>
2026-01-11 18:03:13 +0530

    Add files via upload

---
+++ problem3.py
@@ -0,0 +1,5 @@
+# install a external module and use if to perform operation of your internet
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I am happy to run my first python code")
+engine.runAndWait()
