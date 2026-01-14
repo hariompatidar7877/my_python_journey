8fea600f1a82031ff96336e43cf780f360b02584
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-01-14 08:43:49 +0530

    chapert 1 problem set

--- problem3.py
+++
@@ -1,5 +0,0 @@
-# install a external module and use if to perform operation of your internet
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("I am happy to run my first python code")
-engine.runAndWait()
--- problem4.py
+++
@@ -1,8 +0,0 @@
-# write a python program to print the contents of a directory using the os module search online for the function which does that
-import os
-directory_path = '/'
-contents = os.listdir(directory_path)
-print(contents)
-
-
-# lable the program written in problem 4 with comment
