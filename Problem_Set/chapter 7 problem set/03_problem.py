5d50f4710305f0c48adc789f6a94afe40d268f6f
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-14 17:55:35 +0530

    Create 03_problem.py

---
+++ Problem_Set/chapter 7 problem set/03_problem.py
@@ -0,0 +1,20 @@
+#3. Attempt problem 1 using while loop.
+n = int(input("Enter the number"))
+i=1
+while(i<11):
+    print(f"{n} x {i} = {n*i}")
+    i+=1
+"""
+OUTPUT
+Enter the number5
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50
+"""
