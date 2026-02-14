1923cb6519f9913e42bf29847521e016a7165a3f
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-14 18:38:49 +0530

    .

--- Problem_Set/chapter 7 problem set/09_problem.py
+++ Problem_Set/chapter 7 problem set/09_problem.py
@@ -1,25 +1,38 @@
 """
-Write a program to print the following star pattern:
+9. Write a program to print the following star pattern
 
 forn-3
-*
-**
+***
+* *
 ***
 """
 n = int(input("Enter the number"))
 for i in range(1,n+1):
-    print("*"* i, end="")
-   
+    if(i==1 or i==n):
+      print("*"* n, end="")
+    else:
+     print("*", end="")
+     print(" "*(n-2),end="")
+     print("*", end="")
     print()
 
 
 
 """
 OUTPUT
-Enter the number5
-*
-**
+Enter the number3
 ***
-****
-*****
+* *
+***
+PS C:\Users\hario\OneDrive\Documents\GitHub\my_python_journey> & C:/Users/hario/AppData/Local/Programs/Python/Python314/python.exe "c:/Users/hario/OneDrive/Documents/GitHub/my_python_journey/Problem_Set/chapter 7 problem set/09_problem.py"
+Enter the number9
+*********
+*       *
+*       *
+*       *
+*       *
+*       *
+*       *
+*       *
+*********
 """
