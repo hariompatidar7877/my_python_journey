9b3947877e6c392812407dde4c60a3b0e082d387
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-16 11:51:51 +0530

    Create 02_list_method.py

---
+++ chapters/chapter 4/02_list_method.py
@@ -0,0 +1,28 @@
+friends = ["Apple","orange", 5, 555.4, False, "Akash", "Ved"]
+friends.append("Hariom")
+print(friends)
+l1 = [1,34,62,2,6,11]
+l1.sort()
+print(l1)
+l1.reverse()
+print(l1)
+print(len(l1))
+l1.insert(2,3333)
+print(l1)
+l1.pop(5)
+print(l1)
+l1.remove(34)
+print(l1)
+l1.append(3323)
+print(l1)
+
+"""
+['Apple', 'orange', 5, 555.4, False, 'Akash', 'Ved', 'Hariom']
+[1, 2, 6, 11, 34, 62]
+[62, 34, 11, 6, 2, 1]
+6
+[62, 34, 3333, 11, 6, 2, 1]
+[62, 34, 3333, 11, 6, 1]
+[62, 3333, 11, 6, 1]
+[62, 3333, 11, 6, 1, 3323]
+"""
