a1a4e0db1eb5d347671a061ad69b6d0cbb9b334c
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-16 14:31:04 +0530

    Create 02_problem.py

---
+++ Problem_Set/chapter 4 problem set/02_problem.py
@@ -0,0 +1,27 @@
+marks = []
+mark1 = int(input("Enter your marks: "))
+marks.append(mark1)
+mark2 = int(input("Enter your marks: "))
+marks.append(mark2)
+mark3 = int(input("Enter your marks: "))
+marks.append(mark3)
+mark4 = int(input("Enter your marks: "))
+marks.append(mark4)
+mark5 = int(input("Enter your marks: "))
+marks.append(mark5)
+mark6 = int(input("Enter your marks: "))
+marks.append(mark6)
+marks.sort()
+print(marks)
+
+
+"""
+OUTPUT
+Enter your marks: 89
+Enter your marks: 88
+Enter your marks: 78
+Enter your marks: 91
+Enter your marks: 79
+Enter your marks: 98
+[78, 79, 88, 89, 91, 98]
+"""
