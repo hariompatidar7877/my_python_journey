d8eb96509ddbe95bc6b6084ec684bbe5146e0aae
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-16 14:27:03 +0530

    Create 01_problem.py

---
+++ Problem_Set/chapter 4 problem set/01_problem.py
@@ -0,0 +1,27 @@
+fruit = []
+f1 = input("Enter fruit namr :")
+fruit.append(f1)
+f2 = input("Enter fruit namr :")
+fruit.append(f2)
+f3 = input("Enter fruit namr :")
+fruit.append(f3)
+f4 = input("Enter fruit namr :")
+fruit.append(f4)
+f5 = input("Enter fruit namr :")
+fruit.append(f5)
+f6 = input("Enter fruit namr :")
+fruit.append(f6)
+f7 = input("Enter fruit namr :")
+fruit.append(f7)
+print(fruit)
+
+"""
+Enter fruit namr :apple
+Enter fruit namr :orange
+Enter fruit namr :mango
+Enter fruit namr :banana
+Enter fruit namr :paynapale
+Enter fruit namr :feeg
+Enter fruit namr :guavava
+['apple', 'orange', 'mango', 'banana', 'paynapale', 'feeg', 'guavava']
+"""
