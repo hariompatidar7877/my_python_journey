bf2c624b8e04b65796e11cf671da5c570b226db7
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-17 18:21:03 +0530

    Create 06_problem.py

---
+++ Problem_Set/chapter 5 problem set/06_problem.py
@@ -0,0 +1,32 @@
+s = {}
+name = input("ENter your name : ")
+language = input("Enter ypur language : ")
+s.update({ name : language})
+name = input("ENter your name : ")
+language = input("Enter ypur language : ")
+s.update({ name : language})
+name = input("ENter your name : ")
+language = input("Enter ypur language : ")
+s.update({ name : language})
+name = input("ENter your name : ")
+language = input("Enter ypur language : ")
+s.update({ name : language})
+name = input("ENter your name : ")
+language = input("Enter ypur language : ")
+s.update({ name : language})
+print(s)
+
+"""
+OUTPUT
+ENter your name : hariom
+Enter ypur language : python
+ENter your name : ved
+Enter ypur language : c++
+ENter your name : nitesh 
+Enter ypur language : java
+ENter your name : jiraj
+Enter ypur language : python
+ENter your name : jay
+Enter ypur language : english
+{'hariom': 'python', 'ved': 'c++', 'nitesh': 'java', 'jiraj': 'python', 'jay': 'english'}
+"""
