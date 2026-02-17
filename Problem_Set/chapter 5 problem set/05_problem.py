662403da16d80dae0f2a8d43435065710de1080b
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-17 18:15:53 +0530

    Create 05_problem.py

---
+++ Problem_Set/chapter 5 problem set/05_problem.py
@@ -0,0 +1,16 @@
+s = {}
+print(type(s))
+a = ()
+print(type(a))
+b = set()
+print(type(b))
+c = []
+print(type(c))
+
+"""
+OUTPUT
+<class 'dict'>
+<class 'tuple'>
+<class 'set'>
+<class 'list'>
+"""
