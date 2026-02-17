59f30c595b32a2ba042dffd14e11bdb48ae97ba1
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-17 18:07:26 +0530

    Create 02_problem.py

---
+++ Problem_Set/chapter 5 problem set/02_problem.py
@@ -0,0 +1,29 @@
+s = set()
+n1 = int(input("Enter your number : "))
+s.add(n1)
+n1 = int(input("Enter your number : "))
+s.add(n1)
+n1 = int(input("Enter your number : "))
+s.add(n1)
+n1 = int(input("Enter your number : "))
+s.add(n1)
+n1 = int(input("Enter your number : "))
+s.add(n1)
+n1 = int(input("Enter your number : "))
+s.add(n1)
+n1 = int(input("Enter your number : "))
+s.add(n1)
+print(s)
+
+"""
+OUTPUT
+Enter your number : 12
+Enter your number : 13
+Enter your number : 14
+Enter your number : 15
+Enter your number : 16
+Enter your number : 17
+Enter your number : 18
+{12, 13, 14, 15, 16, 17, 18}
+
+"""
