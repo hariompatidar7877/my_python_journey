cc05c8fc53291c70c661febfb6531181b594bf51
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-17 17:50:43 +0530

    Create 04_set.method.py

---
+++ chapters/chapter 5/04_set.method.py
@@ -0,0 +1,12 @@
+s = { 1,5,32,54,5,5,5,"Hariom"}
+print(s, type(s))
+s.add(56)
+print(s)
+print(len(s))
+print(s.remove(1))
+print(s.clear)
+"""
+OUTPUT
+{32, 1, 5, 54, 'Hariom'} <class 'set'>
+{32, 1, 5, 54, 56, 'Hariom'}
+"""
