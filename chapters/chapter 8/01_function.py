dafc136f4aa8a08535a3686925c40430692bc578
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-19 10:36:23 +0530

    Create 01_function.py

---
+++ chapters/chapter 8/01_function.py
@@ -0,0 +1,10 @@
+# function definition
+def avg ():
+  a = int(input("Enter pour number : "))
+  b = int(input("Enter pour number : "))
+  c = int(input("Enter pour number : "))
+  average = (a+b+c)/3
+  print(average)
+
+avg()  # function call
+print("Thank you")
