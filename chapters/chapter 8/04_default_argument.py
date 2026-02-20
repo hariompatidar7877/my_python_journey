1e00c3da766dee55a7fa18fdeb9e52fc4195714e
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-20 19:13:14 +0530

    Create 04_default_argument.py

---
+++ chapters/chapter 8/04_default_argument.py
@@ -0,0 +1,20 @@
+def goodDay(name, ending = "Thanks!"):
+    print("Good Day\n" + name )
+    print(ending)
+    return "Done!"
+    
+goodDay("Hariom Patidar", "Thank you1")
+A = goodDay("Ved",)
+print(A)
+
+
+"""
+OUTPUT
+Good Day
+Hariom Patidar
+Thank you1
+Good Day
+Ved
+Thanks!
+Done!
+"""
