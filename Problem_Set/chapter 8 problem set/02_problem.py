f130712e0641ebfcce1d4ee0e02213b27b4d7b5a
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-20 19:13:23 +0530

    Create 02_problem.py

---
+++ Problem_Set/chapter 8 problem set/02_problem.py
@@ -0,0 +1,14 @@
+def f_to_c (f):
+    return 5*(f-32)/9
+f = int(input("Enter your temperature : "))
+print(f_to_c(f))
+a = f_to_c(f)
+print(f"{round(a,2)}")
+
+
+"""
+OUTPUT
+Enter your temperature : 456
+235.55555555555554
+235.56
+"""
