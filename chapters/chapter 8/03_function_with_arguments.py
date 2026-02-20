c3aed55e7cdbaf776bd1eb290f3467332bbdc59c
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-20 19:13:09 +0530

    Create 03_function_with_arguments.py

---
+++ chapters/chapter 8/03_function_with_arguments.py
@@ -0,0 +1,21 @@
+def goodDay(name, ending):
+    print("Good Day \n" + name )
+    print(ending)
+    return "Done!"
+    
+goodDay("Hariom Patidar", "Thank you1")
+A = goodDay("Ved", "Thanks")
+print(A)
+
+
+
+"""
+OUTPUT
+Good Day 
+Hariom Patidar
+Thank you1
+Good Day
+Ved
+Thanks
+Done!
+"""
