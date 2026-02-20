b52bd5209337dcc5f1edb95fe615010c4fc81807
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-20 19:13:26 +0530

    Create 04_problem.py

---
+++ Problem_Set/chapter 8 problem set/04_problem.py
@@ -0,0 +1,16 @@
+def sum(n):
+    if(n==0):
+        return 0
+    elif(n==1):
+        return 1
+    else:
+        return sum(n-1) + n
+    
+
+n = int(input("Enter the sum number"))
+print(sum(n))
+
+"""
+Enter the sum number5
+15
+"""
