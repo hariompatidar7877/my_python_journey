f79ae568936c4e245abaac5e0a54d092dce2d2de
HARIOM PATIDAR <hariompatidar6377@gmail.com>
2026-02-20 19:13:29 +0530

    Create 06_problem.py

---
+++ Problem_Set/chapter 8 problem set/06_problem.py
@@ -0,0 +1,12 @@
+def inch_to_cms(inch):
+    return(inch*2.5)
+
+inch = int(input("Enter inch "))
+print(f"The convert inch to cms : {inch_to_cms(inch)}")
+
+
+"""
+OUTPUT
+Enter inch 20
+The convert inch to cms : 50.0
+"""
